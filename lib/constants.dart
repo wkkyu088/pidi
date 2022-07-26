198f8b7d0567b21a79ff13a12aa466783c222db8
wkkyu088 <dnjsrbwls08@gmail.com>
2022-07-27 00:29:21 +0900

    리스트보기 간단하게 구성

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
 
+// 배경색
 Color kWhite = const Color(0xFFFAFAFA);
-Color kGrey = Colors.grey;
+
+// 메인 컬러, 아이콘 등
+Color kBlack = const Color(0xFF262626);
+
+// 비활성화
+Color kGrey = const Color(0xFFBFBFBF);
+
+// appbar 타이틀 밑줄, border 등
+Color kUnderline = const Color(0xFFE7E6E6);
--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -12,7 +12,52 @@ class ListScreen extends StatefulWidget {
 class _ListScreenState extends State<ListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: customAppBar('리스트 보기'), body: const Text('list screen'));
+    final List<String> dates = <String>[
+      '2022-07-25',
+      '2022-07-10',
+      '2022-07-02'
+    ];
+    final List<String> images = <String>['0', '1', '2'];
+    final List<String> titles = <String>[
+      '사진 진짜 마음에 든다 너무 이뻐',
+      '구름이 양털같네',
+      '어떻게 구름이 하나도 없지?'
+    ];
+
+    Widget item(i) {
+      return Column(
+        children: [
+          Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+              alignment: Alignment.centerLeft,
+              child: Text(dates[i], style: const TextStyle(fontSize: 14))),
+          Container(
+            alignment: Alignment.center,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.asset('./assets/images/img${images[i]}.jpg')),
+          ),
+          Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+              alignment: Alignment.centerLeft,
+              child: Text(titles[i], style: const TextStyle(fontSize: 18))),
+        ],
+      );
+    }
+
+    Widget _buildSuggestions() {
+      return ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        itemCount: dates.length,
+        itemBuilder: (context, i) {
+          return item(i);
+        },
+        separatorBuilder: (context, i) => const Divider(),
+      );
+    }
+
+    return Scaffold(appBar: customAppBar('리스트 보기'), body: _buildSuggestions());
   }
 }
