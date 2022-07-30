c41939976ab69814ad41c4bec5dd19786c6e5463
gobi <ohhj1999@gmail.com>
2022-07-30 23:02:26 +0900

    splash screen 생성 완료

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,1 +1,51 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
 
+import '../constants.dart';
+import '../main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 2),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => const MainPage())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kWhite,
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: const [
+                0.65,
+                0.35,
+              ],
+              colors: [
+                kWhite,
+                kUnderline,
+              ],
+            ),
+          ),
+          child: Text('PIDI',
+              style: TextStyle(
+                  color: kBlack, fontSize: 80, fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+}
