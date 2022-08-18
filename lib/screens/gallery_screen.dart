59d5774174fe5728de1e8e6e66b6f67558d44e2f
wkkyu088 <dnjsrbwls08@gmail.com>
2022-08-19 01:34:18 +0900

    이미지 클릭하면 포스트로 이동

--- lib/screens/gallery_screen.dart
+++ lib/screens/gallery_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:pidi/constants.dart';
 import 'package:pidi/models/test.dart';
+import 'package:pidi/screens/detail_screen.dart';
 
 import '../widgets/custom_appbar.dart';
 import '../constants.dart';
@@ -15,72 +16,98 @@ class GalleryScreen extends StatefulWidget {
 
 class _GalleryScreenState extends State<GalleryScreen> {
   // posts에서 이미지 가져올 수 있도록 작성했는데 화면에 이미지가 안 나와서 주석처리
-  // List images = [];
-  // void getImages() {
-  //   for (int i = 0; i < posts.length; i++) {
-  //     for (int j = 0; j < posts[i].images.length; j++) {
-  //       images.add([posts[i].date, posts[i].images[j]]);
-  //       print(posts[i].images[j]);
-  //     }
-  //   }
-  // }
+  // 함수 잘만들었는데 실행을 안해서 안보이던거였어...!! 유레카  -규진
+  List images = [];
+  void getImages() {
+    for (int i = 0; i < posts.length; i++) {
+      for (int j = 0; j < posts[i].images.length; j++) {
+        images.add([posts[i].date, posts[i].images[j]]);
+        // print(posts[i].images[j]);
+      }
+    }
+  }
 
-  final images = [
-    ['2020-02-02', './assets/images/img0.jpg'],
-    ['2020-02-02', './assets/images/img1.jpg'],
-    ['2020-02-02', './assets/images/img2.jpg'],
-    ['2020-02-02', './assets/images/img3.jpg'],
-    ['2020-02-02', './assets/images/img4.jpg'],
-    ['2020-02-02', './assets/images/img0.jpg'],
-    ['2020-02-02', './assets/images/img1.jpg'],
-    ['2020-02-02', './assets/images/img2.jpg'],
-    ['2020-02-02', './assets/images/img3.jpg'],
-    ['2020-02-02', './assets/images/img4.jpg'],
-    ['2020-02-02', './assets/images/img0.jpg'],
-    ['2020-02-02', './assets/images/img1.jpg'],
-    ['2020-02-02', './assets/images/img2.jpg'],
-    ['2020-02-02', './assets/images/img3.jpg'],
-    ['2020-02-02', './assets/images/img4.jpg'],
-    ['2020-02-02', './assets/images/img0.jpg'],
-    ['2020-02-02', './assets/images/img1.jpg']
-  ];
+  // final images = [
+  //   ['2020-02-02', './assets/images/img0.jpg'],
+  //   ['2020-02-02', './assets/images/img1.jpg'],
+  //   ['2020-02-02', './assets/images/img2.jpg'],
+  //   ['2020-02-02', './assets/images/img3.jpg'],
+  //   ['2020-02-02', './assets/images/img4.jpg'],
+  //   ['2020-02-02', './assets/images/img5.jpg'],
+  //   ['2020-02-02', './assets/images/img0.jpg'],
+  //   ['2020-02-02', './assets/images/img1.jpg'],
+  //   ['2020-02-02', './assets/images/img2.jpg'],
+  //   ['2020-02-02', './assets/images/img3.jpg'],
+  //   ['2020-02-02', './assets/images/img4.jpg'],
+  //   ['2020-02-02', './assets/images/img5.jpg'],
+  //   ['2020-02-02', './assets/images/img0.jpg'],
+  //   ['2020-02-02', './assets/images/img1.jpg'],
+  //   ['2020-02-02', './assets/images/img2.jpg'],
+  //   ['2020-02-02', './assets/images/img3.jpg'],
+  //   ['2020-02-02', './assets/images/img1.jpg'],
+  //   ['2020-02-02', './assets/images/img4.jpg'],
+  //   ['2020-02-02', './assets/images/img5.jpg'],
+  //   ['2020-02-02', './assets/images/img0.jpg'],
+  // ];
 
   @override
   Widget build(BuildContext context) {
+    getImages();
+
     return Scaffold(
         appBar: customAppBar('갤러리 보기'),
         body: Container(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 6.0),
           child: MasonryGridView.count(
+            physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics()),
             crossAxisCount: galleryViewColumn.indexOf(true) + 1,
             itemCount: images.length,
             itemBuilder: (BuildContext context, int index) => Container(
               margin:
                   const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-              child: Stack(
-                children: <Widget>[
-                  // 사진
-                  ClipRRect(
-                    borderRadius: kBorderRadius,
-                    child: Image.asset(
-                      images[index][1],
-                      fit: BoxFit.contain,
+              child: GestureDetector(
+                onTap: () {
+                  int idx = -1;
+                  for (int i = 0; i < posts.length; i++) {
+                    if (posts[i].date == images[index][0]) {
+                      idx = i;
+                      break;
+                    }
+                  }
+
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              DetailScreen(post: posts[idx])));
+                },
+                child: Stack(
+                  children: <Widget>[
+                    // 사진
+                    ClipRRect(
+                      borderRadius: kBorderRadius,
+                      child: Image.asset(
+                        images[index][1],
+                        fit: BoxFit.contain,
+                      ),
                     ),
-                  ),
-                  // 날짜 표시줄
-                  Center(
-                      child: Container(
-                    margin: const EdgeInsets.symmetric(vertical: 10.0),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 5.0, horizontal: 10.0),
-                    decoration: BoxDecoration(
-                        color: Colors.black54, borderRadius: kBorderRadius),
-                    child: Text(
-                      images[index][0],
-                      style: TextStyle(color: kWhite, fontSize: kContentS),
-                    ),
-                  )),
-                ],
+                    // 날짜 표시줄
+                    Center(
+                        child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 7.0),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4.0, horizontal: 15.0),
+                      decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.4),
+                          borderRadius: kBorderRadiusL),
+                      child: Text(
+                        images[index][0],
+                        style: TextStyle(color: kWhite, fontSize: kContentS),
+                      ),
+                    )),
+                  ],
+                ),
               ),
             ),
           ),
